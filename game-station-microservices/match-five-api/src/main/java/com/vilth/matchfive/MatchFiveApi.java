fd2dfbbafbba1716b1d33a0708a5f1170e181df7
Vilth83 <thierry.villepreux@gmail.com>
2021-06-26 13:09:46 +0200

    microservices first commit

---
+++ game-station-microservices/match-five-api/src/main/java/com/vilth/matchfive/MatchFiveApi.java
@@ -0,0 +1,13 @@
+package com.vilth.matchfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchFiveApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchFiveApi.class, args);
+	}
+
+}
---
+++ game-station-microservices/match-five-api/src/test/java/com/vilth/matchfive/MatchFiveApiTests.java
@@ -0,0 +1,13 @@
+package com.vilth.matchfive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchFiveApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
